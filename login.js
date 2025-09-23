a71dc3d0105234e0b002c7b3c82780553da32e20
Mari <marisvjv007@gmail.com>
2025-09-23 09:19:28 +0530

    new24

--- login.js
+++ login.js
@@ -54,7 +54,9 @@ function redirectToRoleDashboard(role) {
   };
   window.location.href = redirectMap[role] || "index.html";
 }
-
+function updatePage() {
+  document.getElementById('myDiv').innerHTML = 'The page has been updated!';
+}
 function initLoginForm() {
   const form = document.getElementById("loginForm");
   form.addEventListener("submit", function (e) { e.preventDefault(); handleLogin(); });
